ce979a452159e3aaf3a83a65e17c6523e7a9c689
mrsanjaya <83075210+mrsanjaya@users.noreply.github.com>
2021-04-24 13:17:18 +0700

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'FalahDev_Ongkir',
+    'Ced_OngkirSandhy',
     __DIR__
 );
